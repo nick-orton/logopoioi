e91d68e4392f97ded2f3c526cbf2fab9046dad03
no <no@orton.biz>
2011-12-11 22:41:23 +0000

    saving says ok, but edit fails

--- src/notepad/views/create.clj
+++ src/notepad/views/create.clj
@@ -1,5 +1,6 @@
 (ns notepad.views.create
-  (:require [notepad.views.layouts :as layouts])
+  (:require [notepad.views.layouts :as layouts]
+            [redis.core :as redis])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
@@ -14,9 +15,18 @@
 (defpage "/create" []
   (layouts/common page))
 
+(def SERVER {:host "127.0.0.1" :port 6379 :db 0})
+
 (defpage "/edit/:id" {id :id}
-  (layouts/common page ))
+  (let [resp (redis/with-server SERVER
+               (do
+                 (redis/get (str "notepad:note:" id))))]
+  (layouts/common page [:div resp])))
 
 
 (defpage [:post "/save"] {:keys [box]}
-  (layouts/common page ))
+  (let [resp (redis/with-server SERVER
+               (do
+                 (let [id (redis/incr "notepad:note:id:count")]
+                   (redis/set (str "notepad:note:" id) box) )))]
+  (layouts/common page [:div resp])))
