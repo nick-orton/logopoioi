611a96d453f84c5b9fc879906aadef9ebf670197
no <no@orton.biz>
2011-12-12 01:42:07 +0000

    adding delete functionality

--- src/notepad/views/create.clj
+++ src/notepad/views/create.clj
@@ -9,23 +9,39 @@
 (defn text-area-setting [value]
   (str "set_text_area(\"" value "\");"))
 
-(defn edit-page [contents form-action id]
+(defn create-page []
   [:div
-    [:form#the_form {:action form-action :method "post"} 
+    [:form#the_form {:action "/save" :method "post"} 
+      [:textarea#the_box {:name "box" :rows 10 :cols 80} ]]
+    [:div 
+      [:a#save.btn {:href "#"} "save"] " | " 
+      [:a#create.btn {:href "/create"} "create"]]])
+
+
+(defn edit-page [contents id]
+  [:div
+    [:form#the_form {:action "/edit" :method "post"} 
       [:textarea#the_box {:name "box" :rows 10 :cols 80} contents]
       [:input#hidden_id {:name "id" :type "hidden" :value id}]]
     [:div 
       [:a#save.btn {:href "#"} "save"] " | " 
-      [:a#create.btn {:href "/create"} "create"]]])
+      [:a#create.btn {:href "/create"} "create"] " | "
+      [:a#delete.btn {:href (str "/delete/" id)} "delete"]]])
 
 (defn view-page [contents]
   [:div#view_area 
     [:pre contents]])
 
 (defpage "/create" []
-  (layouts/common (edit-page "" "/save" 0)))
+  (layouts/common (create-page )))
 
 (def SERVER {:host "127.0.0.1" :port 6379 :db 0})
+
+(defpage "/delete/:id" {id :id}
+  (redis/with-server SERVER
+    (do
+      (redis/del (str "notepad:note:" id))
+      (redirect "/create"))))
 
 (defpage "/view/:id" {id :id}
   (redis/with-server SERVER
@@ -36,7 +52,7 @@
 (defpage "/edit/:id" {id :id}
   (redis/with-server SERVER
     (let [resp (redis/get (str "notepad:note:" id))]
-      (layouts/common (edit-page resp "/edit" id)))))
+      (layouts/common (edit-page resp id)))))
 
 (defpage [:post "/edit"] {:keys [box id]}
   (redis/with-server SERVER
