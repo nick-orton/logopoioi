a62bf6a02941ac6fd04c4f8fdbd2f107d2d63000
Nick Orton <nick@orton.biz>
2011-12-17 14:03:11 +0000

    refactoring

--- src/logopoioi/models/logos.clj
+++ src/logopoioi/models/logos.clj
@@ -1,24 +1,28 @@
 (ns logopoioi.models.logos
   (:import [org.apache.hadoop.hbase.client HTable Put Get] 
            [org.apache.hadoop.hbase.util Bytes] 
-           [org.apache.hadoop.hbase HBaseConfiguration]
-)
-  )
+           [org.apache.hadoop.hbase HBaseConfiguration]))
 
-(defn get-table []
+
+(def TABLE-NAME "test")
+(def COLUMN-FAM "cf")
+(def LOG-COL "a")
+
+
+(defn- get-table []
   (let [config (HBaseConfiguration/create)
-        table (HTable. config "test")]
+        table (HTable. config TABLE-NAME)]
     (.setAutoFlush table false);
     (.setWriteBufferSize table (* 1024 1024 12))
-    table
-  ))
+    table))
 
-(defn fetch [id]
-  (let [table (get-table)
-        rowId (Bytes/toBytes id)
-        get-obj (Get. rowId)
-        ]
-    (.addColumn get-obj (Bytes/toBytes "cf") (Bytes/toBytes "a"))
+(defn- get-col [id col]
+ (let [table (get-table)
+        get-obj (Get. (Bytes/toBytes id)) ]
+    (.addColumn get-obj (Bytes/toBytes COLUMN-FAM) (Bytes/toBytes col))
     (let [result (.get table get-obj)]
       (.close table)
       (Bytes/toString (.getValue (.get (.list result) 0))))))
+
+(defn fetch [id]
+  (get-col id LOG-COL))
