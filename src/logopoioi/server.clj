7791424ee5626f3ef6a9edd55134b228fb1fb408
Nick Orton <nick@orton.biz>
2011-12-17 22:54:24 +0000

    moving to port 8091

--- src/logopoioi/server.clj
+++ src/logopoioi/server.clj
@@ -5,6 +5,6 @@
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (Integer. (get (System/getenv) "PORT" "8091"))]
     (server/start port {:mode mode
                         :ns 'logopoioi})))
