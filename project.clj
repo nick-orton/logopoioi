bcbbdd4e6af9cc8af6f5611a3ced1da59e4f0bbb
Nick Orton <nick@orton.biz>
2011-12-17 05:51:37 +0000

    can fetch from hbase

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "A pastebin app"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojars.tavisrudd/redis-clojure "1.3.0"]
+                           [org.apache.hbase/hbase "0.90.4"]
                            [noir "1.2.1"]]
             :main logopoioi.server)
 
