991ea8c52cd363ad07d5e3d66215286141b5c5e1
Nick Orton <orton.nick@gmail.com>
2011-12-17 22:57:28 -0500

    stylish login page

--- resources/public/js/logopoioi.js
+++ resources/public/js/logopoioi.js
@@ -1,6 +1,27 @@
+function focusIf(selector){
+  if($(selector).length != 0){
+    $(selector).focus();
+  } 
+}
+
 $(document).ready(function(){
-  $("#the_box").focus();
+  focusIf("#the_box");
+  focusIf("#pwd_input");
+
   $("#save").click(function(event){
     $("#the_form").submit();      
+  });
+});
+
+
+//Take from http://css-tricks.com/better-password-inputs-iphone-style/
+$(function() {
+  $("#login_form div").append("<div id='letterViewer'>");
+  $("#pwd_input").keypress(function(e) {
+    $("#letterViewer")
+      .html(String.fromCharCode(e.which))
+      .fadeIn(200, function() {
+          $(this).fadeOut(200);
+       });
   });
 });
--- src/logopoioi/views/login.clj
+++ src/logopoioi/views/login.clj
@@ -5,14 +5,30 @@
         [noir.response :only [redirect]]))
 
 (def questions ["I don't know you, B"
-                "What is the airspeed velocity of an unladen sparrow?"])
+                "What is the airspeed velocity of an unladen sparrow?"
+                "What's the frequency Kenneth?"
+                "Who's your Daddy?"
+                "Which way is up?"
+                "Where are my pants?"
+                "Who wears short shorts?" ])
+
+(def error_msgs ["That wasn't right" 
+                 "Try again, son"
+                 "Have you ever seen a baby pigeon? Well, neither have I. I got a hunch they exist"
+                 "It happens sometimes. People just explode. Natural causes."]) 
+
+(def r (java.util.Random.))
+
+(defn random-from [messages]
+  (messages (.nextInt r (count messages))))
 
 (defn login-page [failed?]
   [:div
-    (if failed? [:p  "That wasn't right"] "")
-    [:p (questions 0)]
-    [:form {:action "/login" :method "post"}
-      [:input {:type "password" :name "login"}]]])
+    (if failed? [:p.error_msg  (random-from error_msgs)] "")
+    [:p#question (random-from questions)]
+    [:form#login_form {:action "/login" :method "post"}
+      [:div
+      [:input#pwd_input {:type "password" :name "login"}]]]])
 
 (defpage "/login" []
   (layouts/common (login-page false) ))
