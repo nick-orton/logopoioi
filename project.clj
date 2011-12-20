29a920adb1b60f5ec8a1ecd134fd7e8701f3c2eb
Nick Orton <orton.nick@gmail.com>
2011-12-19 20:25:16 -0500

    adding markdown->html gen with showdown

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "A pastebin app"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.apache.hbase/hbase "0.90.4"]
+                           [org.mozilla/rhino "1.7R3"]
                            [noir "1.2.1"]]
             :main logopoioi.server)
 
---
+++ src/logopoioi/models/markdown.clj
@@ -0,0 +1,14 @@
+(ns logopoioi.markdown
+  (:import (org.mozilla.javascript Context ScriptableObject)))
+
+(defn markdown-to-html [txt]
+  (let [cx (Context/enter)
+        scope (.initStandardObjects cx)
+        input (Context/javaToJS txt scope)
+        script (str (slurp "js/showdown.js")
+                           "new Showdown.converter().makeHtml(input);")]
+    (try
+      (ScriptableObject/putProperty scope "input" input)
+      (let [result (.evaluateString cx scope script "<cmd>" 1 nil)]
+        (Context/toString result))
+      (finally (Context/exit)))))
